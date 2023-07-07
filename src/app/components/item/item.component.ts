b0d81dc1b0aa727dcd412c80664729d23a9f98c8
Lucas <lucasdec88@gmail.com>
2023-07-07 17:51:34 +0100

    OnChanges and OnInit

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
   styleUrls: ['./item.component.css']
 })
-export class ItemComponent implements OnInit {
+export class ItemComponent implements OnInit, OnChanges {
 
+  @Input() item!: Item;
   faPen = faPen;
   faTrash = faTrash
 
   constructor() { }
 
-  ngOnInit(): void { }
+  //onChanges run firt than onInit
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log("ngOnChanges");;
+  }
+
+  //OnInit run after than OnChanges
+  ngOnInit(): void {
+    console.log("ngOnInit");
+  }
 
 }
